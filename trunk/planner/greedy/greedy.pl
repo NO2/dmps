858523a7576064e28b840b300cdf4093f8707e1e
nishant.no2@gmail.com <nishant.no2@gmail.com@df312272-2fa6-3d16-a7f1-e299925ce4ea>
2015-02-13 12:55:25 +0000

    updated planner

--- trunk/planner/greedy/greedy.pl
+++ trunk/planner/greedy/greedy.pl
@@ -1,26 +1,32 @@
 use strict;
 use 5.010;
 use constant H => 5;
-
-open FILE, "<", "gen0.dat" or die $!;
 my @parr;
 my $size;
-chomp($size = <FILE>);
-my ($line,$j,@chars);
-while(chomp($line= <FILE>)) {
-	@chars = split //,$line;
-	for (my $i=0;$i<=$#chars;$i++) {
-		$parr[$j][$i]=$chars[$i];
-		#print $parr[$j][$i];
+for (my $k=0;$k<=10;$k++) {
+	undef @parr;
+	open FILE, "<", "gen$k.dat" or die $!;
+	chomp($size = <FILE>);
+	my ($line,$j,@chars);
+	while(chomp($line= <FILE>)) {
+		@chars = split //,$line;
+		for (my $i=0;$i<=$#chars;$i++) {
+			$parr[$j][$i]=$chars[$i];
+			#print $parr[$j][$i];
+		}
+		$j++;
+		#print "\n";
 	}
-	$j++;
-	#print "\n";
-}
-my $p=0;
-print "0\n";
-for (my $t=0;($t<=($size*3- H)) && ($p<=$size);$t++) {
-	$p=search($p,$t);
-	print "$p\n";
+	close FILE;
+	open FILE, ">", "path$k.dat" or die $!;
+	my $p=0;
+	print FILE "0\n";
+	for (my $t=0;($t<=($size*3- H)) && ($p<=$size) && $p>-1;$t++) {
+		$p=search($p,$t);
+		if ($p>-1) {
+			print FILE "$p\n";
+		}
+	}
 }
 sub search {
 	#move left
@@ -33,8 +39,10 @@ sub search {
 	push @cost,&g($p+1,$t+1,H,1);
 	#print @cost , "\n";
 	my $step=&max(@cost);
-	if ($cost[$step]>-1) {
+	if ($cost[$step]>-1000) {
 		$p+=$step-1;
+	} else {
+		return -1;
 	}
 	return $p;	
 	#stay
@@ -45,36 +53,36 @@ sub g {
 	($p,$t,$d,$a)=@_;
 	#print "sr$p $t $d\n";
 	if ($d==0) {
-		return 0;
+		return $p;
 	}
 	if ($p<0) {
-		return -1;
+		return -1000;
 	} elsif ($p>0 && $p<=$size) {
 		#print $parr[$t][$p], " ", $parr[$t][$p]eq"X", "\n";
-		if ($parr[$t][$p]eq"X") {
-			return -1;
+		if ($parr[$t][$p]eq"X") { #hit
+			return -1000;
 		}
-		if (($parr[$t][$p-$a] eq "X") && ($parr[$t-1][$p] eq "X")) {
-			return -1;
+		if (($parr[$t][$p-$a] eq "X") && ($parr[$t-1][$p] eq "X")) { #diagonal trap
+			return -1000;
 		}
 	}
 	my @cost;
 	push @cost,&g($p-1,$t+1,$d-1);
 	push @cost,&g($p,$t+1,$d-1);
 	push @cost,&g($p+1,$t+1,$d-1);
+	#print @cost , "\n";
 	my $step=&max(@cost);
-	if ($cost[$step]>-1) {
+	if ($cost[$step]>-1000) {
 		$g+=$cost[$step];
 		if($p<=$size+1) {
-			if ($parr[$t+1][$p]eq"X") {
-				$g+=1;
-			} else {
-				$g+=2*$p;
+			if ($parr[$t+1][$p]eq"X") { #close to a hit
+				$g-=5;
 			}
+			$g+=$p-abs($a)-$t;
 		} 
 		return $g;
 	}
-	return -1;		
+	return -1000;		
 	#p,t,d
 }
 sub max {
