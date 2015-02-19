4e03c67d8380fe7e1f339b8906f5adcca3051b06
nishant.no2@gmail.com <nishant.no2@gmail.com@df312272-2fa6-3d16-a7f1-e299925ce4ea>
2015-02-19 10:00:06 +0000

    oct irl update

--- planner/irl/oct/loadp.m
+++ planner/irl/oct/loadp.m
@@ -1,18 +1,30 @@
-H=5;
-ws=H*H+4*H+6;
+global H=5;
+global ws=H*H+4*H+6;
 [fdg, err] = fopen("gen0.dat");
 [fdp, err] = fopen("path0.dat");
-gsize=strtod(mgetl(fdg,1));
-grid=mgetl(fdg);
-expert=strtod(mgetl(fdp));
-epls=zeros(size(expert,1)-1,1);
+global gsize=str2double(fgetl(fdg));
+global grid;
+for i=1:gsize*3
+	grid(i,:)=fgetl(fdg);
+endfor
+#grid=mgetl(fdg);
+exp=fgetl(fdp);
+i=1;
+global expert;
+while (exp!=-1)
+	expert(i)=str2double(exp);
+	i=i+1;
+	exp=fgetl(fdp);
+endwhile
+#expert=strtod(mgetl(fdp));
+global epls=zeros(size(expert,1)-1,1);
 for i=2:size(expert,1)
     epls(i-1)=expert(i)-expert(i-1);
-end
-//use part to search character
-//define feature vectors for a state
-//gen random weights
-//find optimal strategy for weights ->write RL code add to policy list
-//maximize weights for diff V
+endfor
+#//use part to search character
+#//define feature vectors for a state
+#//gen random weights
+#//find optimal strategy for weights ->write RL code add to policy list
+#//maximize weights for diff V
 fclose(fdg);
 fclose(fdp);
