0552217eee114ff625cde95d3926afee1c5347d9
nishant.no2@gmail.com <nishant.no2@gmail.com@df312272-2fa6-3d16-a7f1-e299925ce4ea>
2015-03-10 12:53:19 +0000

    updated irlga2

---
+++ trunk/planner/greedy/greedys2.pl
@@ -0,0 +1,89 @@
+use strict;
+use 5.010;
+use constant H => 5;
+
+open FILE, "<", "gen1.dat" or die $!;
+my @parr;
+my $size;
+chomp($size = <FILE>);
+my ($line,$j,@chars);
+while(chomp($line= <FILE>)) {
+	@chars = split //,$line;
+	for (my $i=0;$i<=$#chars;$i++) {
+		$parr[$j][$i]=$chars[$i];
+		#print $parr[$j][$i];
+	}
+	$j++;
+	#print "\n";
+}
+close FILE;
+my $p=0;
+print "0\n";
+for (my $t=0;($t<=($size*3- H)) && ($p<=$size) && $p>-1;$t++) {
+	$p=search($p,$t);
+	if ($p>-1) {
+		print "$p\n";
+	}
+}
+sub search {
+	#move left
+	my ($p,$t);
+	my @cost;
+	($p,$t)=@_;
+	print "sr$p $t\n";
+	push @cost,&g($p-1,$t+1,H,-1);
+	push @cost,&g($p,$t+1,H,0);
+	push @cost,&g($p+1,$t+1,H,1);
+	print @cost , "\n";
+	my $step=&max(@cost);
+	$p+=$step-1;
+	return $p;	
+	#stay
+	#move right
+}
+sub g {
+	my ($p,$t,$d,$g,$a);
+	($p,$t,$d,$a)=@_;
+	#print "sr$p $t $d\n";
+	if ($d==0) {
+		return $p-$a;
+	}
+	if ($p<0) {
+		$g-=$d*1000;
+		return $g;
+	} elsif ($p>0 && $p<=$size) {
+		#print $parr[$t][$p], " ", $parr[$t][$p]eq"X", "\n";
+		if ($parr[$t][$p]eq"X") { #hit
+			$g-=1000;
+			#return $g;
+		}
+		if (($parr[$t][$p-$a] eq "X") && ($parr[$t-1][$p] eq "X")) { #diagonal trap
+			$g-=1000;
+			#return $g;
+		}
+	}
+	my @cost;
+	push @cost,&g($p-1,$t+1,$d-1,-1);
+	push @cost,&g($p,$t+1,$d-1,0);
+	push @cost,&g($p+1,$t+1,$d-1,1);
+	#print @cost , "\n";
+	my $step=&max(@cost);
+	$g+=$cost[$step];
+	if($p<=$size+1) {
+		if ($parr[$t+1][$p]eq"X") { #close to a hit
+			$g-=5;
+		}
+		$g+=$p-abs($a)-1;
+	} 
+	return $g;		
+	#p,t,d
+}
+sub max {
+	my $max=0;
+	for (my $i=1;$i<=$#_;$i++) {
+		if ($_[$i]>$_[$max]) {
+			$max=$i;
+		}
+	}
+	return $max;
+}
